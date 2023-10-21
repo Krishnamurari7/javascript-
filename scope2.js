01d20df3c3b359868a02091d827a46d4a055e197
Krishna murari kumar <139735281+Krishnamurari7@users.noreply.github.com>
2023-10-21 23:37:32 +0530

    Add files via upload

---
+++ scope2.js
@@ -0,0 +1,40 @@
+function one(){
+    const username = "krishna"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+
+    }
+    // console.log(website);
+
+    two()
+}
+// one()
+
+if(true){
+    const username = "krishna"
+    if(username === "krishna"){
+        const website = "youtube"
+        console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+addone(5)
+
+console.log(addone(5));
+
+function addone(num){
+    return num+1
+}
+
+
+
+
+const addtwo = function(num){
+    return num+2
+}
+ addtwo(5)
